eb79f956783e9dbda2e0362156639f8316c73f1d
Jorge Escalante <jescalantegt@gmail.com>
2021-02-27 12:33:01 -0600

    Corrige los errores marcados por el linter

--- pages/explorar.vue
+++ pages/explorar.vue
@@ -1,53 +1,64 @@
 <template>
   <div class="estilo-base flex">
     <div class="h-screen w-screen grid gap-0 grid-rows-4 grid-cols-12  barra-navegacion " >
-        <div class="row-span-4 col-span-2 pr-8">
-            <div>
-                <div class="mt-20">
-                    <h1>
-                        Icono
-                    </h1>
-                </div>
-                <div class="mt-10">
-                    <div class="rounded-full py-3 px-6">
-                        <a href="https://www.google.com" class="no-underline tm-2" style="color: #6b7779"><i class="mr-2">icono</i>Explorar</a>
-                        <br>
-                        <a href="https://www.google.com" class="no-underline mt-4" style="color: #6b7779"><i class="mr-2">icono</i>Estaciones</a>
-                        <br>
-                        <a href="https://www.google.com" class="no-underline mt-4" style="color: #6b7779"><i class="mr-2">icono</i>Comunidades</a>
-                        <br>
-                        <a href="https://www.google.com" class="no-underline mt-4" style="color: #6b7779"><i class="mr-2">icono</i>Artistas</a>
-                    </div>
-                </div>
-                <div class="mt-48 ml-10 mr-8 px-2 pt-4 pb-8 rounded box-content w-48 shadow-2xl" style="background: #ffffff;">
-                  <i>
-                    Icono
-                  </i>
-                  <p class="text-center">Registrate para obtener más beneficios</p>
-                  <button class="bg-green-500 px-4 py-2 text-white">ÚNETE AHORA</button>
-                </div>
+      <div class="row-span-4 col-span-2 pr-8">
+        <div>
+          <div class="mt-20">
+            <h1>
+              Icono
+            </h1>
+          </div>
+          <div class="mt-10">
+            <div class="rounded-full py-3 px-6">
+              <a href="https://www.google.com" class="no-underline tm-2" style="color: #6b7779"><i class="mr-2">icono</i>Explorar</a>
+              <br>
+              <a href="https://www.google.com" class="no-underline mt-4" style="color: #6b7779"><i class="mr-2">icono</i>Estaciones</a>
+              <br>
+              <a href="https://www.google.com" class="no-underline mt-4" style="color: #6b7779"><i class="mr-2">icono</i>Comunidades</a>
+              <br>
+              <a href="https://www.google.com" class="no-underline mt-4" style="color: #6b7779"><i class="mr-2">icono</i>Artistas</a>
             </div>
-        </div>
-        <div class="col-span-8 row-span-4 rounded-l-lg contenido-fondo py-12 px-8">
-          <div class="box-content mb-10">
-            <h2 class="text-left">Explorar</h2>
           </div>
-          <div class="box-content w-75 text-left pt-20 pl-8 pb-2 rounded-lg" style="background: red;">
-            <h3 class="text-2xl">VEDI Quetzaltenango</h3>
-            <p class="text-sm"><i>i</i>  Guatemala</p>
-            <p class="text-sm">
-              <i> i</i>
-              <i> i</i>
-                ## personas
+          <div class="mt-48 ml-10 mr-8 px-2 pt-4 pb-8 rounded box-content w-48 shadow-2xl" style="background: #ffffff;">
+            <i>
+              Icono
+            </i>
+            <p class="text-center">
+              Registrate para obtener más beneficios
             </p>
-          </div>
-          <div>
-            si
+            <button class="bg-green-500 px-4 py-2 text-white">
+              ÚNETE AHORA
+            </button>
           </div>
         </div>
-        <div class="col-span-2 row-span-4 contenido-fondo">
-          no
+      </div>
+      <div class="col-span-8 row-span-4 rounded-l-lg contenido-fondo py-12 px-8">
+        <div class="box-content mb-10">
+          <h2 class="text-left">
+            Explorar
+          </h2>
         </div>
+        <div class="box-content w-75 text-left pt-20 pl-8 pb-2 rounded-lg" style="background: red;">
+          <h3 class="text-2xl">
+            VEDI Quetzaltenango
+          </h3>
+          <p class="text-sm">
+            <i>i</i>
+            Guatemala
+          </p>
+          <p class="text-sm">
+            <i>i</i>
+            <i>i</i>
+            ## personas
+          </p>
+        </div>
+        <div>
+          si
+        </div>
+      </div>
+      <div class="col-span-2 row-span-4 contenido-fondo">
+        no
+      </div>
     </div>
   </div>
 </template>
