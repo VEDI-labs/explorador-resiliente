0d00e6415484c39249465b35cb1a7eb2042e4122
Jorge Escalante <jescalantegt@gmail.com>
2021-03-02 08:44:03 -0600

    Crea página de artistas

---
+++ pages/artists/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="flex flex-col items-start w-full">
+    <h1>Artistas</h1>
+    <section class="flex flex-col items-start w-full">
+      <h4 class="mb-8">
+        VEDI Guatemala
+      </h4>
+      <div class="grid grid-cols-8 w-full gap-x-2 gap-y-8">
+        <ArtistCard full-name="Renato Cifuentes" class="col-span-2" />
+        <ArtistCard full-name="Alejandra González" class="col-span-2" />
+        <ArtistCard full-name="Fernando Martínez" class="col-span-2" />
+        <ArtistCard full-name="Ester Samayoa" class="col-span-2" />
+        <ArtistCard full-name="Renato Cifuentes" class="col-span-2" />
+        <ArtistCard full-name="Alejandra González" class="col-span-2" />
+        <ArtistCard full-name="Fernando Martínez" class="col-span-2" />
+        <ArtistCard full-name="Ester Samayoa" class="col-span-2" />
+      </div>
+      <button class="self-center mt-8">
+        Ver más artistas
+      </button>
+    </section>
+    <section class="mt-16 flex flex-col items-start w-full">
+      <h4 class="mb-8">
+        VEDI México
+      </h4>
+      <div class="grid grid-cols-8 w-full gap-x-2 gap-y-8">
+        <ArtistCard full-name="Renato Cifuentes" class="col-span-2" />
+        <ArtistCard full-name="Alejandra González" class="col-span-2" />
+        <ArtistCard full-name="Fernando Martínez" class="col-span-2" />
+        <ArtistCard full-name="Ester Samayoa" class="col-span-2" />
+        <ArtistCard full-name="Renato Cifuentes" class="col-span-2" />
+        <ArtistCard full-name="Alejandra González" class="col-span-2" />
+        <ArtistCard full-name="Fernando Martínez" class="col-span-2" />
+        <ArtistCard full-name="Ester Samayoa" class="col-span-2" />
+      </div>
+      <button class="self-center mt-8">
+        Ver más artistas
+      </button>
+    </section>
+  </div>
+</template>
+<script>
+import ArtistCard from '~/components/ArtistCard'
+export default {
+  components: {
+    ArtistCard
+  }
+}
+</script>
